a4614d3994c0bcca1edd089bd3b9ba07ef31dc19
yaacovk10 <yaacovana@gmail.com>
2023-11-13 00:02:52 +0200

    add front 2

--- front/static/myscript.js
+++ front/static/myscript.js
@@ -1,12 +1,27 @@
 let my_cart = []
 let products = []
-// const MY_SERVER = "http://127.0.0.1:8000/"
-const MY_SERVER = "https://dj-supermarket.onrender.com/"
+const MY_SERVER = "http://127.0.0.1:8000/"
+// const MY_SERVER = "https://dj-supermarket.onrender.com/"
+const cart_data = JSON.parse(localStorage.getItem("my_cart"))
         const load_product = async ()=>{
             const res = await axios.get(MY_SERVER + "products")
             products = res.data
-            console.log(res.data);
-            display.innerHTML= `<div class="row row-cols-1 row-cols-md-5 g-4">`+
+            display_products()
+        }
+        const load_cart = () => {
+
+          if (cart_data === null) 
+          {console.log("cart empty")
+              return 
+          }
+          else {
+              cart = cart_data
+              console.log("cart loaded success")
+              display_cart()
+          }
+      }
+        const display_products = ()=>{
+          display.innerHTML= `<div class="row row-cols-1 row-cols-md-5 g-4">`+
             products.map((prod, idx) => `<div class="col">
             <div class="card">
               <img src=${prod.img} class="card-img-top" alt="...">
@@ -21,10 +36,28 @@ const MY_SERVER = "https://dj-supermarket.onrender.com/"
         }
         const buy = (idx) =>{
             prod = products[idx]
-            console.log("product item" + prod.desc)
-            my_cart.push({"description" : prod.desc , "price" : prod.price, "amount" :  1 , "image": prod.img})
-            console.log(my_cart);
-            cart.innerHTML =  `<div class="row row-cols-1 row-cols-md-5 g-4">`+
+            // console.log(prod)
+            console.log(prod.id)
+            console.log(my_cart)
+            // console.log(my_cart.filter(x=>x.id === prod.id))
+            if (my_cart.filter(x=>x.id === prod.id).length>0){
+              current_item = my_cart.filter(x=>x.id === prod.id)[0]
+              current_item.amount += 1
+            }
+            else{
+              // console.log("product item" + prod.desc)
+              my_cart.push({"description" : prod.desc , "price" : prod.price, "amount" :  1 , "image": prod.img, "id":prod.id})
+              // console.log(my_cart);
+            }
+            //add to local storage
+            localStorage.setItem("my_cart", JSON.stringify(my_cart))
+            console.table(my_cart)
+            display_cart()
+            
+        }
+        const display_cart =() =>{
+          cart.innerHTML = "hello"
+          cart.innerHTML =  `<div class="row row-cols-1 row-cols-md-5 g-4">`+
             my_cart.map((cart, idx) => `<div class="col">
             <div class="card">
               <img src=${cart.image} class="card-img-top" alt="...">
@@ -38,3 +71,4 @@ const MY_SERVER = "https://dj-supermarket.onrender.com/"
           </div>`) + `</div>`
         }
         load_product()
+        load_cart()
